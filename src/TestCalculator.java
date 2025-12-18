6ff602fa1f56142c74aa520b06ba0342fa943de3
HUAWEİ <s230446033@stu.thk.edu.tr>
2025-12-18 14:34:20 +0300

    İnitialize commit

---
+++ src/TestCalculator.java
@@ -0,0 +1,9 @@
+public class TestCalculator {
+    public static void main(String[] args){
+        Calculator c1=new Calculator();
+        System.out.println(c1.add(5,6));
+        System.out.println(c1.add(5.2,6.3));
+        System.out.println(c1.add(9,6,11));
+    }
+
+}
