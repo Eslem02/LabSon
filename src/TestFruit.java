c5e771f6cc701e2f7fb66bf2a8971cf8a5e09faf
HUAWEİ <s230446033@stu.thk.edu.tr>
2025-12-18 14:56:09 +0300

    İnitialize commit

---
+++ src/TestFruit.java
@@ -0,0 +1,9 @@
+public class TestFruit {
+    public static void main(String[] args){
+        Citrus c1=new Citrus("Mandalin",5.0,6.1);
+        Orange o1=new Orange("lemon",5.6,8.3,10);
+
+        c1.describe();
+        o1.describe();
+    }
+}
